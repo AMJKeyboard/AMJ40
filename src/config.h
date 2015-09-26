8008b79d11589b7fa61dca6792105e143c7f9a74
Han Chen <chen1324+git@gmail.com>
2015-09-26 11:28:02 +0800

    update usb DEVICE_VER to 2

--- src/config.h
+++ src/config.h
@@ -22,7 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x6072
-#define DEVICE_VER      0x0001
+#define DEVICE_VER      0x0002
 #define MANUFACTURER    Han Chen
 #define PRODUCT         AMJ40
 #define DESCRIPTION     t.m.k. keyboard firmware for AMJ40
